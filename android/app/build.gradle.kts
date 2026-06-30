25646ae2c0cfe9f65ef0da7f77ed6daa855f84e9
marketingddbn9-arch <your-email@gmail.com>
2026-06-30 18:44:25 +0530

    Fix: Correct Kotlin DSL syntax for build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,21 +5,23 @@ plugins {
 }
 
 android {
-    namespace "com.example.bmh_app"
-    compileSdkVersion flutter.compileSdkVersion
+    namespace = "com.example.bmh_app"
+    compileSdk = flutter.compileSdkVersion.toInteger()
     ndkVersion = "28.2.13676358"
-	
-defaultConfig {
-        minSdkVersion flutter.minSdkVersion
-        targetSdkVersion flutter.targetSdkVersion
-        versionCode flutterVersionCode.toInteger()
-        versionName flutterVersionName
+
+    defaultConfig {
+        applicationId = "com.example.bmh_app"
+        minSdk = flutter.minSdkVersion.toInteger()
+        targetSdk = flutter.targetSdkVersion.toInteger()
+        versionCode = flutterVersionCode.toInteger()
+        versionName = flutterVersionName
     }
+
     compileOptions {
-    isCoreLibraryDesugaringEnabled = true
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
